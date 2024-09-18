bc067393712a12d5156bfdcd4f17e5ba29cb3d80
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-09-18 08:44:47 -0300

    Hover mouse basic collision detection

--- script/graphics.js
+++ script/graphics.js
@@ -128,6 +128,67 @@ fetch("asset/scene3.obj")
 	console.error(e);
 });
 
+document.getElementById("glcanvas").addEventListener("mousemove", function(event_){
+	let mouseX = event_.offsetX;
+	let mouseY = event_.offsetY;
+	
+	const canvasWidth = event_.target.offsetWidth;
+	const canvasHeight = event_.target.offsetHeight;
+	
+	let fNear = 0.1;
+	let fFov = 45.0;
+	let fFovRad = Math.tan(fFov * 0.5 / 180.0 * Math.PI);
+	
+	const campos = new Vector3(-cameraPos[0], -cameraPos[1], -cameraPos[2]);
+	const camdir = new Vector3(-cameraDir[0], -cameraDir[1], cameraDir[2]);
+	
+	let vecRight = camdir.cross(new Vector3(0.0, 1.0, 0.0));
+	vecRight = vecRight.scale(1 / vecRight.magnitude()); // Normalize.
+	let vecUpwards = vecRight.cross(camdir); // Normalized already since it is the cross product of 2 normalized orthoginal vectors.
+	
+	let cam2mouse = camdir.scale(fNear).add(vecRight.scale(2 * (canvasWidth / canvasHeight) * fNear * fFovRad * ((mouseX - canvasWidth / 2) / canvasWidth))).add(vecUpwards.scale(2 * fNear * fFovRad * ((canvasHeight / 2 - mouseY) / canvasHeight)));
+	let mouse3d = campos.add(cam2mouse);
+	
+	let cam2mouseNormalized = cam2mouse.scale(1 / cam2mouse.magnitude());
+	
+	let intersections = [];
+	
+	for(let i = 0; i < bodies.length; i++){
+		let body = bodies[i];
+		body.selected = false;
+		for(let j = 0; j < body.model.vertexCount / 3; j++){
+			let v1 = new Vector3(...body.model.vertices.slice(9 * j + 0, 9 * j + 3));
+			let v2 = new Vector3(...body.model.vertices.slice(9 * j + 3, 9 * j + 6));
+			let v3 = new Vector3(...body.model.vertices.slice(9 * j + 6, 9 * j + 9));
+			
+			let vec1 = v1.add(v2.scale(-1.0));
+			let vec2 = v2.add(v3.scale(-1.0));
+			let vec3 = v3.add(v1.scale(-1.0));
+			
+			let n1 = v1.add(campos.scale(-1.0)).cross(vec1);
+			let n2 = v2.add(campos.scale(-1.0)).cross(vec2);
+			let n3 = v3.add(campos.scale(-1.0)).cross(vec3);
+			
+			if(cam2mouse.dot(n1) > 0 && cam2mouse.dot(n2) > 0 && cam2mouse.dot(n3) > 0 && cam2mouse.dot(vec1.cross(vec2)) < 0){
+				// https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection
+				let n = vec1.cross(vec2);
+				let parallel = cam2mouse.dot(n);
+				if(parallel != 0){
+					let d = v1.add(campos.scale(-1.0)).dot(n) / parallel;
+					// let intersection = campos.add(cam2mouse.scale(d));
+					intersections.push([i, d]);
+				}
+				break;
+			}
+		}
+	}
+	
+	if(intersections.length > 0){
+		intersections.sort((a, b) => {return a[1] - b[1];});
+		bodies[intersections[0][0]].selected = true;
+	}
+});
+
 
 // Flip image pixels into the bottom-to-top order that WebGL expects.
 gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
