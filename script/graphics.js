1354b8609e23b32f89fc5c2637b5a874d3ccdbc6
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-01 02:03:19 -0300

    Animated container for animation elements
    Contains: canvas element, GL context, 3D camera, canvas size, model arrays, lighting.

--- script/graphics.js
+++ script/graphics.js
@@ -12,8 +12,6 @@ function main(){
 		return;
 	}
 	
-	let canvasSize = F32Vector(2, [canvas.offsetWidth, canvas.offsetHeight]);
-	
 	Promise.all([
 		fetch("script/shader/vertexColor.vs"),
 		fetch("script/shader/vertexColor.fs"),
@@ -42,7 +40,9 @@ function main(){
 		// Collect all the info needed to use the shader program.
 		// Look up which attribute our shader program is using
 		// for aVertexPosition and look up uniform locations.
-		const programInfo = {
+		const Animated = {
+			canvas: canvas,
+			gl: gl,
 			program: shaderProgram,
 			attribLocations: {
 				vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
@@ -61,26 +61,27 @@ function main(){
 				LightDirection: gl.getUniformLocation(shaderProgram, "uLightDirection"),
 				LightColor: gl.getUniformLocation(shaderProgram, "uLightColor"),
 			},
-		};
-		
-		
-		let camera = new Camera(45.0, 0.1, 100.0, canvasSize[1] / canvasSize[0], [0.2, -0.4, -1.2, 1.0], [0.0, -Math.PI * 90 / 180, 0.0, 1.0]);
-		
-		let camRangeH = Math.PI * 25 / 180;
-		let camrangeV = Math.PI * 25 / 180;
-		
-		let LightGlobal = {
-			diffuse: F32Vector(3, [0.3, 0.3, 0.3]),
-			directional: {
-				direction: F32Vector(3, [-1.0, -10.0, -2.0]).normalize(),
-				color: F32Vector(3, [1.0, 1.0, 1.0]),
+			canvasSize: F32Vector(2, [canvas.offsetWidth, canvas.offsetHeight]),
+			camera: new Camera(45.0, 0.1, 100.0, canvas.offsetHeight / canvas.offsetWidth, [0.2, -0.4, -1.2, 1.0], [0.0, -Math.PI * 90 / 180, 0.0, 1.0]),
+			cameraThreshold: [],
+			lightGlobal: {
+				diffuse: F32Vector(3, [0.3, 0.3, 0.3]),
+				directional: {
+					direction: F32Vector(3, [-1.0, -10.0, -2.0]).normalize(),
+					color: F32Vector(3, [1.0, 1.0, 1.0]),
+				},
+			},
+			scenery: [],
+			bodies: [],
+			modelLoad(models, container){
+				for(let i = 0; i < models.length; i++){
+					container.push(new Body(F32Vector(4, pen_F32Matrix.W1), F32Vector(4, pen_F32Matrix.W1), models[i], this.gl));
+				}
 			},
 		};
 		
 		// Here's where we call the routine that builds all the
 		// objects we'll be drawing.
-		let bodies = [];
-		
 		fetch("asset/bodies.obj").then((response) => {
 			if(!response.ok){
 				throw new Error(`Response status: ${response.status}`);
@@ -89,14 +90,10 @@ function main(){
 		}).then((text) =>
 			pen_obj.obj_load(text)
 		).then((loaded) => {
-			for(let i = 0; i < loaded.length; i++){
-				bodies.push(new Body(F32Vector(4, pen_F32Matrix.W1), F32Vector(4, pen_F32Matrix.W1), loaded[i], gl));
-			}
+			Animated.modelLoad(loaded, Animated.bodies);
 		}).catch((e) => {
 			console.error(e);
 		});
-		
-		let scenery = [];
 		
 		fetch("asset/scenery.obj").then((response) => {
 			if(!response.ok){
@@ -106,39 +103,37 @@ function main(){
 		}).then((text) =>
 			pen_obj.obj_load(text)
 		).then((loaded) => {
-			for(let i = 0; i < loaded.length; i++){
-				scenery.push(new Body(F32Vector(4, pen_F32Matrix.W1), F32Vector(4, pen_F32Matrix.W1), loaded[i], gl));
-			}
+			Animated.modelLoad(loaded, Animated.scenery);
 		}).catch((e) => {
 			console.error(e);
 		});
 		
-		[camRangeH, camrangeV] = update_canvasResize(canvas, camera, canvasSize);
+		update_canvasResize(Animated);
 		window.addEventListener("resize", function(event_){
-			[camRangeH, camrangeV] = update_canvasResize(canvas, camera, canvasSize);
+			update_canvasResize(Animated);
 		});
 		
 		canvas.addEventListener("mousemove", function(event_){
 			let mouseX = event_.offsetX;
 			let mouseY = event_.offsetY;
 			
-			camera.rotate([(mouseY / canvasSize[1] - 0.5) * camrangeV, -Math.PI * 90 / 180 + (mouseX / canvasSize[0] - 0.5) * camRangeH, 0.0, 1.0]);
+			Animated.camera.rotate([(mouseY / Animated.canvasSize[1] - 0.5) * Animated.cameraThreshold[1], -Math.PI * 90 / 180 + (mouseX / Animated.canvasSize[0] - 0.5) * Animated.cameraThreshold[0], 0.0, 1.0]);
 			
-			const campos = new F32Vector(3, [-camera.position[0], -camera.position[1], -camera.position[2]]);
-			const camdir = new F32Vector(3, [-camera.direction[0], -camera.direction[1], camera.direction[2]]);
+			const campos = new F32Vector(3, [-Animated.camera.position[0], -Animated.camera.position[1], -Animated.camera.position[2]]);
+			const camdir = new F32Vector(3, [-Animated.camera.direction[0], -Animated.camera.direction[1], Animated.camera.direction[2]]);
 			
 			let vecRight = camdir.copy().cross(pen_F32Matrix.Y1).normalize();
 			let vecUpwards = vecRight.copy().cross(camdir); // Normalized already since it is the cross product of 2 normalized orthoginal vectors.
 			
-			let cam2mouse = camdir.copy().scale(camera.Znear).add(vecRight.copy().scale(2 * (canvasSize[0] / canvasSize[1]) * camera.Znear * camera.FoVratio * ((mouseX - canvasSize[0] / 2) / canvasSize[0]))).add(vecUpwards.copy().scale(2 * camera.Znear * camera.FoVratio * ((canvasSize[1] / 2 - mouseY) / canvasSize[1])));
+			let cam2mouse = camdir.copy().scale(Animated.camera.Znear).add(vecRight.copy().scale(2 * (Animated.canvasSize[0] / Animated.canvasSize[1]) * Animated.camera.Znear * Animated.camera.FoVratio * ((mouseX - Animated.canvasSize[0] / 2) / Animated.canvasSize[0]))).add(vecUpwards.copy().scale(2 * Animated.camera.Znear * Animated.camera.FoVratio * ((Animated.canvasSize[1] / 2 - mouseY) / Animated.canvasSize[1])));
 			let mouse3d = campos.copy().add(cam2mouse);
 			
 			let cam2mouseNormalized = cam2mouse.copy().normalize();
 			
 			let intersections = [];
 			
-			for(let i = 0; i < bodies.length; i++){
-				let body = bodies[i];
+			for(let i = 0; i < Animated.bodies.length; i++){
+				let body = Animated.bodies[i];
 				body.selected = false;
 				for(let j = 0; j < body.model.vertexCount / 3; j++){
 					const d = collision_LineTriangle(
@@ -162,7 +157,7 @@ function main(){
 			
 			if(intersections.length > 0){
 				intersections.sort((a, b) => {return a[1] - b[1];});
-				bodies[intersections[0][0]].selected = true;
+				Animated.bodies[intersections[0][0]].selected = true;
 			}
 		});
 		
@@ -175,7 +170,7 @@ function main(){
 			let deltaTime = now - then;
 			then = now;
 			
-			drawScene(gl, programInfo, camera, LightGlobal, scenery.concat(bodies), deltaTime);
+			drawScene(gl, Animated, Animated.camera, Animated.lightGlobal, Animated.scenery.concat(Animated.bodies), deltaTime);
 
 			requestAnimationFrame(render);
 		}
@@ -190,30 +185,25 @@ function main(){
 main();
 
 
-function update_canvasResize(glcanvas, camera, canvasSize)
+function update_canvasResize(Animated)
 {
-	const gl = glcanvas.getContext("webgl");
-	
-	canvasSize[0] = glcanvas.offsetWidth;
-	canvasSize[1] = glcanvas.offsetHeight;
+	Animated.canvasSize[0] = Animated.canvas.offsetWidth;
+	Animated.canvasSize[1] = Animated.canvas.offsetHeight;
 	
 	// Update canvas size.
-	glcanvas.width = canvasSize[0];
-	glcanvas.height = canvasSize[1];
+	Animated.canvas.width = Animated.canvasSize[0];
+	Animated.canvas.height = Animated.canvasSize[1];
 	
 	// Apply viewport resolution.
-	gl.viewport(0, 0, canvasSize[0], canvasSize[1]);
+	Animated.gl.viewport(0, 0, Animated.canvasSize[0], Animated.canvasSize[1]);
 	
 	// Adjust horizontal and vertical camera direction ranges.
-	camRangeH = Math.PI * (37 + 32 * ((canvasSize[1] / canvasSize[0]) - (9 / 16))) / 180;
-	camrangeV = Math.PI * (35 - 22 * ((canvasSize[1] / canvasSize[0]) - (9 / 16))) / 180;
+	Animated.cameraThreshold = [Math.PI * (37 + 32 * ((Animated.canvasSize[1] / Animated.canvasSize[0]) - (9 / 16))) / 180, Math.PI * (35 - 22 * ((Animated.canvasSize[1] / Animated.canvasSize[0]) - (9 / 16))) / 180];
 	
 	// Move camera position.
-	camera.position[0] = 0.0 - 0.8 * ((canvasSize[1] / canvasSize[0]) - (9 / 16));
+	Animated.camera.position[0] = 0.0 - 0.8 * ((Animated.canvasSize[1] / Animated.canvasSize[0]) - (9 / 16));
 	
 	// Update projection matrix.
-	camera.aspectRatio = canvasSize[1] / canvasSize[0];
-	camera.project();
-	
-	return [camRangeH, camrangeV];
+	Animated.camera.aspectRatio = Animated.canvasSize[1] / Animated.canvasSize[0];
+	Animated.camera.project();
 }
