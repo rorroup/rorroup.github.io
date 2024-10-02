fe975290c86603fbc21d26d84c16f302192f3dc5
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-02 04:38:49 -0300

    F32Matrix rework
    Replaced Float32Array array type in F32Matrix class to improve performance.
    Used composition instead of inheritance (for now).
    Fixed size array slowed down page display significantly.
    WebGL would not accept resizable ArrayBuffer in a class where matrix multiplication inherently may change the matrix size making it necessary to initialize a new Float32Array opun passing the matrix defeating the whole purpose to use it as the Base Class in the first place.
    TODO: Rename class and revisit inheritance.

--- script/lib/pen_F32Matrix.js
+++ script/lib/pen_F32Matrix.js
@@ -1,9 +1,5 @@
 
-const pen_F32Matrix = {
-	MAX_ELEMENTS: 64,
-};
-
-class F32Matrix extends Float32Array
+class F32Matrix
 {
 	/*
 		Simple and flexible fixed size Float32 Matrix class in column-main-order.
@@ -21,35 +17,36 @@ class F32Matrix extends Float32Array
 		}else{
 			rows = columns = Number(size);
 		}
-		super(new ArrayBuffer(4 * rows * columns, {maxByteLength: 4 * pen_F32Matrix.MAX_ELEMENTS}));
-		if(data != false) this.set(data);
+		this.data = Array(rows * columns);
+		this.data.fill(0.0);
+		if(data != false) this.data.splice(0, data.length, ...(data.slice(0, this.data.length)));
 		this.rows = rows;
 		this.columns = columns;
 	}
 	
 	copy(){
-		return new F32Matrix([this.rows, this.columns], this);
+		return new F32Matrix([this.rows, this.columns], this.data);
 	}
 	
 	// General linear algebra operations.
 	
 	scale(scalar){
-		for(let i = 0; i < this.length; i++){
-			this[i] *= scalar;
+		for(let i = 0; i < this.data.length; i++){
+			this.data[i] *= scalar;
 		}
 		return this;
 	}
 	
 	add(other){
-		for(let i = 0; i < this.length; i++){
-			this[i] += other[i];
+		for(let i = 0; i < this.data.length; i++){
+			this.data[i] += other.data[i];
 		}
 		return this;
 	}
 	
 	substract(other){
-		for(let i = 0; i < this.length; i++){
-			this[i] -= other[i];
+		for(let i = 0; i < this.data.length; i++){
+			this.data[i] -= other.data[i];
 		}
 		return this;
 	}
@@ -60,14 +57,13 @@ class F32Matrix extends Float32Array
 			for(let j = 0; j < this.rows; j++){
 				let result = 0.0;
 				for(let k = 0; k < this.columns; k++){
-					result += this[k * this.columns + j] * other[i * other.rows + k];
+					result += this.data[k * this.columns + j] * other.data[i * other.rows + k];
 				}
 				matrix.push(result);
 			}
 		}
 		this.columns = other.columns;
-		this.buffer.resize(4 * this.rows * this.columns);
-		this.set(matrix);
+		this.data = matrix;
 		return this;
 	}
 	
@@ -84,31 +80,32 @@ class F32Matrix extends Float32Array
 	
 	dot(other){
 		let result = 0.0;
-		for(let i = 0; i < this.length; i++){
-			result += this[i] * other[i];
+		for(let i = 0; i < this.data.length; i++){
+			result += this.data[i] * other.data[i];
 		}
 		return result;
 	}
 	
 	cross(other){
-		this.set([
-			this[1] * other[2] - other[1] * this[2],
-			this[2] * other[0] - other[2] * this[0],
-			this[0] * other[1] - other[0] * this[1],
-		]);
+		this.data = [
+			this.data[1] * other.data[2] - other.data[1] * this.data[2],
+			this.data[2] * other.data[0] - other.data[2] * this.data[0],
+			this.data[0] * other.data[1] - other.data[0] * this.data[1],
+			1.0
+		].slice(0, this.data.length);
 		return this;
 	}
 	
 	// 4D Vector component access.
 	
-	get x(){return this[0];}
-	set x(val){this[0] = val;}
-	get y(){return this[1];}
-	set y(val){this[1] = val;}
-	get z(){return this[2];}
-	set z(val){this[2] = val;}
-	get w(){return this[3];}
-	set w(val){this[3] = val;}
+	get x(){return this.data[0];}
+	set x(val){this.data[0] = val;}
+	get y(){return this.data[1];}
+	set y(val){this.data[1] = val;}
+	get z(){return this.data[2];}
+	set z(val){this.data[2] = val;}
+	get w(){return this.data[3];}
+	set w(val){this.data[3] = val;}
 }
 
 function F32Vector(size, data = false)
@@ -116,8 +113,10 @@ function F32Vector(size, data = false)
 	return new F32Matrix([size, 1], data);
 }
 
-pen_F32Matrix.X1 = F32Vector(4, [1.0, 0.0, 0.0, 1.0]);
-pen_F32Matrix.Y1 = F32Vector(4, [0.0, 1.0, 0.0, 1.0]);
-pen_F32Matrix.Z1 = F32Vector(4, [0.0, 0.0, 1.0, 1.0]);
-pen_F32Matrix.W1 = F32Vector(4, [0.0, 0.0, 0.0, 1.0]);
-pen_F32Matrix.ZERO = F32Vector(4, [0.0, 0.0, 0.0, 0.0]);
+const pen_F32Matrix = {
+	X1: F32Vector(4, [1.0, 0.0, 0.0, 1.0]),
+	Y1: F32Vector(4, [0.0, 1.0, 0.0, 1.0]),
+	Z1: F32Vector(4, [0.0, 0.0, 1.0, 1.0]),
+	W1: F32Vector(4, [0.0, 0.0, 0.0, 1.0]),
+	ZERO: F32Vector(4, [0.0, 0.0, 0.0, 0.0]),
+};
