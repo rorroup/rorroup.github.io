a212d6c9cea683eda92bd27a4b425fa07d9acc32
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-07 09:17:40 -0300

    FrameBuffer draw target moved up

--- script/graphics.js
+++ script/graphics.js
@@ -71,6 +71,35 @@ function main(){
 			return;
 		}
 		
+		// create to render to
+		const targetTextureWidth = canvas.offsetWidth;
+		const targetTextureHeight = canvas.offsetHeight;
+		const targetTexture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+		
+		// define size and format of level 0
+		const level = 0;
+		const internalFormat = gl.RGBA;
+		const border = 0;
+		const format = gl.RGBA;
+		const type = gl.UNSIGNED_SHORT_4_4_4_4;
+		const data = null;
+		gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+			targetTextureWidth, targetTextureHeight, border,
+			format, type, data);
+		
+		// set the filtering so we don't need mips
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		
+		// Create and bind the framebuffer
+		const fb = gl.createFramebuffer();
+		gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+		
+		// attach the texture as the first color attachment
+		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, level);
+		
 		// Collect all the info needed to use the shader program.
 		// Look up which attribute our shader program is using
 		// for aVertexPosition and look up uniform locations.
@@ -306,39 +335,6 @@ function main(){
 		// Flip image pixels into the bottom-to-top order that WebGL expects.
 		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 		
-		
-		    // create to render to
-    const targetTextureWidth = canvas.offsetWidth;
-    const targetTextureHeight = canvas.offsetHeight;
-    const targetTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-     
-    
-      // define size and format of level 0
-      const level = 0;
-      const internalFormat = gl.RGBA;
-      const border = 0;
-      const format = gl.RGBA;
-      const type = gl.UNSIGNED_SHORT_4_4_4_4;
-      const data = null;
-      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                    targetTextureWidth, targetTextureHeight, border,
-                    format, type, data);
-     
-      // set the filtering so we don't need mips
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    
-		
-		    // Create and bind the framebuffer
-    const fb = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-     
-    // attach the texture as the first color attachment
-    const attachmentPoint = gl.COLOR_ATTACHMENT0;
-    gl.framebufferTexture2D(
-        gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, level);
 		
 		// Create a buffer for the square's positions.
   const positionBuffer = gl.createBuffer();
