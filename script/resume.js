b12b7c268dc649fe2384149f0d7e5b45a807d98c
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-28 15:30:03 -0300

    resume update

--- script/resume.js
+++ script/resume.js
@@ -184,7 +184,7 @@ function resume_buildResume(language){
 		resume_sectionLeft(resume_data, "Personal", resume_buildInformation),
 		resume_sectionLeft(resume_data, "Links", resume_buildLinks),
 		resume_sectionLeft(resume_data, "Software", resume_buildSkillTable),
-		resume_sectionLeft(resume_data, "Programming", resume_buildSkillTable),
+		resume_sectionLeft(resume_data, "Skills", resume_buildSkillTable),
 		resume_sectionLeft(resume_data, "Language", resume_buildSkillTable),
 	);
 	document.getElementById("resume_right").replaceChildren(
