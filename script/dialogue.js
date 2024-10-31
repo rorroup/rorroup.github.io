0c6acd1136994cdf31d549d1bf40a3ffbce3855c
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-31 12:50:39 -0300

    dialogue revision

--- script/dialogue.js
+++ script/dialogue.js
@@ -103,6 +103,7 @@ function* setInput(attributes_ = {type: "text", placeholder: "Write your answer"
 	document.getElementById("dialogue_prompt").replaceChildren(input_prompt);
 	input.focus();
 	yield response;
+	input_prompt.id = "";
 	document.getElementById("dialogue_prompt").replaceChildren();
 	return response;
 }
@@ -227,7 +228,7 @@ function* npc_information(){
 							yield* animateNPC("happy");
 							yield* writeDialogue(`<p>Of course...<br/>So it is '${escapeHTML(playerName)}' right?</p>`);
 							yield* animateNPC("angry");
-							yield* writeDialogue(`<h1>You think I'm stupid?!</h1>`);
+							yield* writeDialogue(`<h1>You think I'm <span style="font-size: 150%; color: red;">stupid?!</span></h1>`);
 							yield* writeDialogue(`<h1>Get out!</h1>`);
 							location.reload();
 							return;
@@ -239,28 +240,28 @@ function* npc_information(){
 							yield* writeDialogue(`<p>${escapeHTML(playerName)}.<br/>I see...</p>`);
 							yield* animateNPC("talk");
 							playerName = "Larry";
-							yield* writeDialogue(`<p>It is a good name. I think it is long to the point it becomes unmanageable though.<br/>So I think I'll just call you ${playerName} for the time being.</p>`);
+							yield* writeDialogue(`<p>It is a good name. I think it is <span style="font-size: 150%;">long</span> to the point it becomes unmanageable though.<br/>So I think I'll just call you ${playerName} for the time being.</p>`);
 							break;
 						}else if(playerName.length == 1){
 							yield* animateNPC("talk");
-							yield* writeDialogue(`<p>${escapeHTML(playerName)}.<br/>It's a bit short, but what do I even know right?</p>`);
+							yield* writeDialogue(`<p>${escapeHTML(playerName)}.<br/>It's a bit <span style="font-size: 60%;">short</span>, but what do I even know right?</p>`);
 							break;
 						}else if(playerName.toLowerCase() == "penicilina"){
 							yield* animateNPC("surprise");
 							yield* writeDialogue(`<p>Your name is</p><h1>${escapeHTML(playerName)}</h1><p>???</p>`);
 							yield* animateNPC("happy");
-							yield* writeDialogue(`<p>Heh.<br/>Nice try.<br/>But I'm the one and only Penicilina so you simply can not be Penicilina.<br/>You better pick another name.</p>`);
+							yield* writeDialogue(`<p>Heh.<br/>Nice try.<br/>But I'm the <span style="font-size: 120%;">one and only</span> <span style="font-size: 180%;">Penicilina</span> so you simply can not be Penicilina.<br/>You better pick another name.</p>`);
 						}else{
 							yield* animateNPC("talk");
-							yield* writeDialogue(`<p>${escapeHTML(playerName)}.<br/>That's a good name.</p>`);
+							yield* writeDialogue(`<p>${escapeHTML(playerName)}.<br/>That's a <span style="font-size: 120%; color: #44FF33;">good</span> name.</p>`);
 							break;
 						}
 					}
 				}
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>My name is Rodrigo Urrutia. I am <b>almost</b> an electrical Engineer, and <b>kind of</b> a self taught programmer.<br/>Penicilina is the nick I would use on the internet, for playing video games for example. You can think of it as my
+				yield* writeDialogue(`<p>My name is Rodrigo Urrutia. I am an electrical Engineer and <i>kind of</i> a self taught programmer.<br/>Penicilina is the nick I would use on the internet, for playing video games for example. You can think of it as my
 				<span class="dropdown_container">
-					<i class="dropdown_trigger">chuunibyou</i>
+					<span class="dropdown_trigger"><i>chuunibyou</i></span>
 					<span class="dropdown_content">
 						<span>
 							Chūnibyō is a Japanese colloquial term typically used to describe early teens who have grandiose delusions, who desperately want to stand out, and who have convinced themselves that they have hidden knowledge or secret powers.<br/> Source: <a href="https://en.wikipedia.org/wiki/Ch%C5%ABniby%C5%8D" target="_blank">Wikipedia</a>
@@ -271,30 +272,37 @@ function* npc_information(){
 				introduced = true;
 				yield* setTalker(`<h2>Penicilina</h2>`);
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>You can find my complete build, or as you may call it: my <i>resume</i>, on that incredible statistics poster on the wall ahead this hallway.<br/>That one was written in HTML and is native to this page, but you can find the link to its original pdf version inside it, alongside some other personal links. You can change its language inside it as well.</p>`);
+				yield* writeDialogue(`<p>You can find my complete
+				<span class="dropdown_container">
+					<span class="dropdown_trigger" style="font-size: 120%;"><b>build</b></span>
+					<span class="dropdown_content">
+						<span>resume</span>
+					</span>
+				</span>
+				on that <i>artistic?</i> <span style="background-color: #000000; padding: 0.2em 0.4em;"><span style="color: #FF3333">stati</span><span style="color: #33FF33">stics p</span><span style="color: #3355FF">oster</span></span> on the wall.<br/>That one was written in HTML and is native to this page, but you can find the link to its original pdf version inside it, alongside some other personal links. You can toggle its language inside it as well.</p>`);
 				break;
 			case 1:
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>Glad you asked. This is my personal web page.</p>`);
+				yield* writeDialogue(`<p>Glad you asked.<br/>This is my <span style="font-size: 120%;">personal</span> <span style="font-size: 160%;">web page &#x1F4BB;&#128512;&#128512;</span>.</p>`);
 				yield* animateNPC("talk");
 				yield* writeDialogue(`<p>I learned basic Python on my first year at University, but I didn't really think much of programming at the time. I found it confusing and kind of pointless.</p>`);
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>Then, one day I watched a Python programming tutorial out of pure boredom, but that time I actually understood its power as a tool for solving actual problems.<br/>That really picked up my interest and made me wonder about the possibilities.</p>`);
+				yield* writeDialogue(`<p>Then, one day I watched a Python programming tutorial out of pure boredom. The result was a simple program, yet it was clearly working software, a self contained program that actually fulfilled a purpose, though limited it was clearly on the right path meaning it could be continually expanded and improved upon. That time I was able to understand programming's power as a tool for solving actual problems.<p>`);
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>I dove right down the snake hole, where the more I would learn the more interesting scripts I would be able to write, but also the harder problems I would encounter, forcing me to go deeper every time. I began to have quite a bit of fun and programming quickly became my hobby. Until I inevitably reached the Python performance wall.</p>`);
+				yield* writeDialogue(`<p>That really picked up my interest and made me wonder if I could do the same. I dove right down the snake hole, where the more I would learn the more interesting scripts I would be able to write, but also the harder problems I would encounter, forcing me to go deeper every time. I began to have quite a bit of fun and programming quickly became my hobby. Until I inevitably reached the Python performance wall.</p>`);
 				yield* animateNPC("talk");
 				yield* writeDialogue(`<p>I decided to learn a new faster language. And from that point onwards for me it was either learning more programming or learning a programming language that can do other things.<br/>Though in the end I came to the realization that in all this time I had mostly been programming simple things, like scripts for simple file organization for my friends or myself, or just plain test scripts.<br/>So I decided to code something a little bit more real, something I could show to people.</p>`);
 				yield* animateNPC("talk");
-				yield* writeDialogue(`<p>That is what this page is, written completely by my hand, a simple program with a basic interface and some information. Being so simple it doesn't use frameworks, external dependencies nor third party libraries because that is the way I learned to program in the first place after all.<br/>Though that means there isn't any art, because despite me being a man of many talents, artistic endeavor is not one of them, so everything had to be built using basic geometric primitives, namely rectangles and circles. To be expected coming from someone who studied electrical engineering anyways. It was either that or exposing you to infamous
+				yield* writeDialogue(`<p>That is what this page is, written completely by my hand, a simple program with a basic interface and some information. Being so simple it doesn't use frameworks, external dependencies nor third party libraries because that is how I learned to program in the first place.<br/>Though that means it looks rather simple because, despite me being a man of many talents, artistic endeavor is not one of them, so everything was built using basic geometric primitives resulting in infamous 
 				<span class="dropdown_container">
-					<i class="dropdown_trigger">programmer art</i>
+					<span class="dropdown_trigger" style="color: #FF0000; background-color: #000000;">programmer art</span>.
 					<span class="dropdown_content">
 						<span>
 							Programmer art refers to temporary assets added by the programmer to test functionality.<br/> Source: <a href="https://en.wikipedia.org/wiki/Programmer_art" target="_blank">Wikipedia</a>
 						</span>
 					</span>
 				</span>
-				.</p>`);
+				To be expected from someone who studied electrical engineering anyways.</p>`);
 				break;
 			case 2:
 				break;
