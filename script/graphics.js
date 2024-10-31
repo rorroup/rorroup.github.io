5db5e100ba4b05fdc87b60dd608d17e2224ed254
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-10-31 20:33:19 -0300

    createFramebuffer moved into pen_canvasgl.js

--- script/graphics.js
+++ script/graphics.js
@@ -453,46 +453,6 @@ function main(){
 	});
 }
 
-
-main();
-
-function createFramebuffer(gl, size)
-{
-	// create to render to
-	const targetTextureWidth = size[0];
-	const targetTextureHeight = size[1];
-	const targetTexture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, targetTexture);
-	
-	// define size and format of level 0
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const border = 0;
-	const format = gl.RGBA;
-	const type = gl.UNSIGNED_SHORT_4_4_4_4;
-	const data = null;
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-		targetTextureWidth, targetTextureHeight, border,
-		format, type, data);
-	
-	// set the filtering so we don't need mips
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	
-	// Create and bind the framebuffer
-	const fb = gl.createFramebuffer();
-	gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-	
-	// attach the texture as the first color attachment
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, level);
-	
-	return {
-		framebuffer: fb,
-		texture: [targetTexture],
-	};
-}
-
 function bookClose(rootID, animation_)
 {
 	animation_.animationID = requestAnimationFrame(animation_.animationStart); // Resume drawing.
@@ -506,3 +466,5 @@ function bookClose(rootID, animation_)
 		root.style.display = "none";
 	});
 }
+
+main();
--- script/lib/pen_canvasgl.js
+++ script/lib/pen_canvasgl.js
@@ -119,6 +119,43 @@ function loadTexture(gl, url){
 	return texture;
 }
 
+function createFramebuffer(gl, size)
+{
+	// create to render to
+	const targetTextureWidth = size[0];
+	const targetTextureHeight = size[1];
+	const targetTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+	
+	// define size and format of level 0
+	const level = 0;
+	const internalFormat = gl.RGBA;
+	const border = 0;
+	const format = gl.RGBA;
+	const type = gl.UNSIGNED_SHORT_4_4_4_4;
+	const data = null;
+	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+		targetTextureWidth, targetTextureHeight, border,
+		format, type, data);
+	
+	// set the filtering so we don't need mips
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	
+	// Create and bind the framebuffer
+	const fb = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+	
+	// attach the texture as the first color attachment
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, level);
+	
+	return {
+		framebuffer: fb,
+		texture: [targetTexture],
+	};
+}
+
 class Camera
 {
 	constructor(FoV, Znear, Zfar, aspectRatio, position = false, rotation = false)
