c7805113b797431351c449558acfa701a4dbeb44
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-11-26 20:05:48 -0300

    general cleaning

--- script/graphics.js
+++ script/graphics.js
@@ -579,7 +579,7 @@ function projection()
 					0.0, 0.0, 0.0, -cornerFar.x, -cornerFar.y, -cornerFar.z,
 				];
 				
-				Animated.cursorBG.src = "asset/Z.png";
+				this.cursorBG.src = "asset/Z.png";
 				
 				this.calculatePoint();
 				
@@ -609,8 +609,7 @@ function projection()
 			draw_3d(){
 				gl.clearColor(0.6, 0.6, 0.6, 1.0); // Background color
 				gl.clearDepth(1.0); // Clear everything
-				// gl.enable(gl.DEPTH_TEST); // Enable depth testing
-				// gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+				gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 				
 				// Clear the canvas before we start drawing on it.
 				gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -637,6 +636,7 @@ function projection()
 					]),
 				};
 				
+				// vertexColor0
 				gl.useProgram(this.glProgram.Fig3D_color.program);
 				
 				gl.uniformMatrix4fv(
@@ -704,7 +704,7 @@ function projection()
 				gl.drawArrays(gl.TRIANGLES, 0, 36);
 				
 				
-				
+				// texture0
 				gl.useProgram(this.glProgram.Fig3D_texture.program);
 				
 				gl.uniformMatrix4fv(
