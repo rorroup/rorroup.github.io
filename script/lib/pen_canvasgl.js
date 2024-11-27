54f6b98b060e2ca332e95f4ce7b80e70c67d71e0
Penicilina <83550767+rorroup@users.noreply.github.com>
2024-11-26 22:05:17 -0300

    Camera rotate fix

--- script/lib/pen_canvasgl.js
+++ script/lib/pen_canvasgl.js
@@ -184,10 +184,9 @@ class Camera
 	}
 	
 	rotate(rotation){
-		this.rotation.set(rotation);
-		this.rotation[0] = Math.min(Math.PI / 2, Math.max(-Math.PI / 2, this.rotation[0]));
-		this.rotation[1] = ((this.rotation[1] * 100000000) % (2 * Math.PI * 100000000)) / 100000000;
-		this.rotation[2] = ((this.rotation[2] * 100000000) % (2 * Math.PI * 100000000)) / 100000000;
+		this.rotation[0] = Math.min(Math.PI / 2, Math.max(-Math.PI / 2, rotation[0]));
+		this.rotation[1] = ((rotation[1] * 100000000) % (2 * Math.PI * 100000000)) / 100000000;
+		this.rotation[2] = ((rotation[2] * 100000000) % (2 * Math.PI * 100000000)) / 100000000;
 		this.direction.set([-Math.sin(this.rotation[1]) * Math.cos(this.rotation[0]), Math.sin(this.rotation[0]), -Math.cos(this.rotation[1]) * Math.cos(this.rotation[0]), 1.0]);
 	}
 	
